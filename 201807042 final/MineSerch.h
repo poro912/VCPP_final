d1283982d0d63aa6e1c02dbf3a88ea12ff830852
poro912 <angus99@naver.com>
2021-12-06 22:14:46 +0900

    최종 작동 장소

---
+++ 201807042 final/MineSerch.h
@@ -0,0 +1,47 @@
+#pragma once
+#include "PAGE.h"
+
+class MineSerch :PAGE
+{
+private:
+	LABEL* TITLE;
+	LABEL* time_label;
+	LABEL* remain_label;
+	LABEL* find_label;
+
+	int x;	// °¡·Î Ä­ ¼ö
+	int y;	// ¼¼·Î Ä­ ¼ö
+
+	int mine;	//remain = serch - mine
+	int serch;
+
+	vector<vector<BUTTON>> map;
+
+public:
+	MineSerch();
+	~MineSerch();
+
+	int proc(
+		HWND		hWnd,
+		UINT		message,
+		WPARAM		wParam,
+		LPARAM		lParam
+	);
+
+
+	void paint(
+		HDC			hdc
+	);
+}
+
+
+
+
+/*int start_button(
+	HWND hWnd,
+	UINT message,
+	WPARAM wParam,
+	LPARAM lParam
+);
+*/
+
